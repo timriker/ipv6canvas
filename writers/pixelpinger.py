44bbfca41191867993bb9507c0aafe288db1b879
Tim Riker <timriker@gmail.com>
2023-06-14 17:08:24 +0000

    updates and writers

---
+++ writers/pixelpinger.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# https://git.sr.ht/~jae/PixelPinger
+
+# This program sends IPv6 pings to a target in order to change pixels.
+# It was made during the IPv6Canvas experiment.
+
+from platform import system
+from sys import argv
+from socket import socket, AF_INET6, SOCK_RAW, IPPROTO_ICMPV6
+
+from PIL import Image
+
+base_address = "2607:fa18:9ffe:4"
+pixels = []
+
+sock = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6)
+
+def make_address(x, y, r, g, b):
+    final_address = f"{base_address}:{x:02x}{y:02x}:{r:02x}:{g:02x}:{b:02x}"
+
+    return final_address
+
+def do_ping(dest_addr, timeout=1):
+    print(f"Rendering current: {dest_addr}", end="\r")
+
+    try:
+        # Raw sockets, who cares, it's fast
+        sock.sendto(b"\x80\0\0\0\0\0\0\0", (dest_addr, 0))
+    except:
+        # We don't care about failing
+        # The pixel is usually re-written in next pass
+        pass
+
+def write_to_file(filename):
+    file = open(filename, "w")
+    for addr in pixels:
+        file.write(f"{addr}\n")
+    file.close()
+
+def make_image_and_start(ox, oy, image_path, to_file=False, filename="image.txt"):
+    image = Image.open(image_path).convert("RGBA")
+    max_w, max_h = image.size
+
+    curr_w, curr_h = 0, 0
+
+    start_w = int(ox)
+    start_h = int(oy)
+
+    max_w -= 1
+    max_h -= 1
+
+    # Now, let's convert the image
+    while True:
+        coordinates = x, y = curr_w, curr_h
+        r, g, b, a = image.getpixel(coordinates)
+
+        print(f"Converting: {x},{y} -> {r}:{g}:{b}/{a}", end="\r")
+
+        # Check for alpha (transparency support)
+        if a > 0:
+            pixels.append(make_address(curr_w + start_w, curr_h + start_h, r, g, b))
+
+        # Basic printer that goes left-> right and starts again
+        if curr_h == max_h:
+            curr_h = 0
+            curr_w += 1
+        else:
+            curr_h += 1
+
+        if curr_w == max_w and curr_h == max_h:
+            curr_h = start_h
+            curr_w = start_w
+            break
+
+    print("Starting the render")
+
+    if not to_file:
+        #while True:
+        for addr in pixels:
+            do_ping(addr)
+    else:
+        write_to_file(filename)
+
+
+if __name__ == "__main__":
+    if not argv or len(argv) < 4 or "--help" in argv:
+        print(
+            "Usage: pixelpinger.py <offset x> <offset y> <image file> [true/false generate to file] [filename]"
+        )
+        exit(1)
+
+    print(argv)
+
+    if len(argv) > 1 and len(argv) < 6:
+        make_image_and_start(argv[1], argv[2], argv[3])
+    elif len(argv) == 6:
+        make_image_and_start(argv[1], argv[2], argv[3], argv[4], argv[5])
---
+++ writers/write_image.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+from pathlib import Path
+from typing import List
+from random import randrange
+import subprocess
+
+#DOMAIN_OFFSET = (155, 78-12)
+DOMAIN_OFFSET = (20, 20)
+
+def make_address(x: int, y: int, r: int, g: int, b: int) -> str:
+    return f"2607:fa18:9ffe:4:{x:02x}{y:02x}:{r:02x}:{g:02x}:{b:02x}"
+
+def image_to_addresses(image_path: Path, x_offset: int, y_offset: int) -> List[str]:
+    # Load the image
+    image = Image.open(image_path)
+    image = image.convert('RGBA')
+    x_offset = randrange(0, 256 - image.width);
+    y_offset = randrange(0, 256 - image.height);
+
+    # Convert to address list
+    addresses = []
+    for x in range(image.width):
+        for y in range(image.height):
+            # Get bitmap pixel
+            r, g, b, a = image.getpixel((x, y))
+            if a != 0:
+                addresses.append(make_address(x + x_offset, y + y_offset, r, g, b))
+
+    return addresses
+
+def ping_address(address: str):
+    print(f"Pinging {address}")
+    subprocess.Popen(["ping", "-n", "-q", "-c", "1", address])
+
+def main():
+    addresses = image_to_addresses(Path(__file__).parent / "Tux-64.png", *DOMAIN_OFFSET)
+
+    # Update the display
+    for address in addresses:
+        ping_address(address)
+
+if __name__ == "__main__":
+    main()
